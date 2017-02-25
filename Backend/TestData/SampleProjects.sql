ca017be52ffa433b3bba24cdf0607d5becf098e1
Jing Yin Ong <theyinvincible@Jings-MacBook-Pro-2.local>
2017-02-25 16:21:02 +0800

    Added 'description' attribute to Projects; Added Projects 41-80

--- Backend/TestData/SampleProjects.sql
+++ Backend/TestData/SampleProjects.sql
@@ -1,3 +1,83 @@
+INSERT INTO project VALUES ('41', 'Voyatouch', '2017-02-11', 39, 'Technology', 456200, 'Drag this device across any surface and it will map out the surface texture on your laptop.');
+
+INSERT INTO project VALUES ('42', 'Tres-Zap', '2017-01-23', 53, 'Technology', 183648, 'The most efficient way of draining the sap from rubber trees');
+
+INSERT INTO project VALUES ('43', 'Solarbreeze', '2016-12-11', 55, 'Technology', 22300, 'Portable solar powered fan which you can carry around with you, keeping yourself cool at all times.');
+
+INSERT INTO project VALUES ('44', 'Bin It', '2016-07-24', 11, 'Technology', 10914, 'Bin that automatically opens when your friends approach it because it knows that they are trash.');
+
+INSERT INTO project VALUES ('45', 'Juice Cleansers', '2016-10-23', 40, 'Technology', 458994, 'Your juice cleanser can now calculate the calories and list the benefits of each juice you make.');
+
+INSERT INTO project VALUES ('46', 'Kaler', '2016-12-01', 37, 'Technology', 41614, 'Waffle machine in the shape of a kale leaf, make all your food look like kale and pretend that you are eating healthy.');
+
+INSERT INTO project VALUES ('47', 'Coriander Picker', '2016-10-08', 31, 'Technology', 262494, 'Picks out all Coriander leaves from your food.');
+
+INSERT INTO project VALUES ('48', 'Clicker Cube', '2016-09-26', 11, 'Design', 114112, 'The ultimate weapon against boredom.');
+
+INSERT INTO project VALUES ('49', '2 Second Lashes', '2016-12-29', 41, 'Fashion', 268956, 'Falsies engineered for you to apply in 2 seconds, no longer.');
+
+INSERT INTO project VALUES ('50', 'Wrapsafe', '2016-06-09', 50, 'Environment', 40000, 'Condoms made of recyclable material.');
+
+INSERT INTO project VALUES ('51', 'Tea Stream', '2016-08-05', 22, 'Food', 38337, 'Get a steady tea supply for one year.');
+
+INSERT INTO project VALUES ('52', 'Lava-osa', '2016-03-08', 90, 'Entertainment', 69430, 'Like the lava lamp you had as a kid, but this time with real lava.');
+
+INSERT INTO project VALUES ('53', 'TransparentVision', '2017-01-04', 37, 'Entertainment', 869120, 'TV made of glass, see-through when it is turned off.');
+
+INSERT INTO project VALUES ('54', 'Gembucket', '2016-11-10', 27, 'Fashion', 19921, 'Super efficient way of storing your jewellery with multiple compartments for every type.');
+
+INSERT INTO project VALUES ('55', 'Unknockabe Glass', '2016-09-01', 47, 'Technology', 25430, 'The glass that will be never knocked down.');
+
+INSERT INTO project VALUES ('56', 'The Ordinary Bag', '2016-07-11', 30, 'Fashion', 20943, 'Just a normal backpack. No fancy gimmicks.');
+
+INSERT INTO project VALUES ('57', 'Beer knife', '2016-12-13', 61, 'Design', 18359, 'Swiss knives fitted with a beer opener.');
+
+INSERT INTO project VALUES ('58', 'Lego Batman shirts', '2016-06-05', 24, 'Fashion', 8350, 'Shirts designed with Lego Batman themes.');
+
+INSERT INTO project VALUES ('59', 'ReRouter', '2016-11-06', 39, 'Technology', 477000, 'Instead of using Google Maps to find the fastest way of going somewhere, use ReRouter to find the most scenic route.');
+
+INSERT INTO project VALUES ('60', 'Ab-sinthe', '2016-12-02', 67, 'Food', 49351, 'Alcohol which gives you abs.');
+
+INSERT INTO project VALUES ('61', 'Filter Water', '2016-12-10', 16, 'Technology', 27084, 'Attach this to your taps and voila you will have drinkable water.');
+
+INSERT INTO project VALUES ('62', 'Tresome', '2016-11-04', 77, 'Entertainment', 30000, 'Grow three trees in this.');
+
+INSERT INTO project VALUES ('63', 'Dotainer', '2016-06-18', 23, 'Design', 4618, 'Roll this over any surface and you will get a consistent dot pattern.');
+
+INSERT INTO project VALUES ('64', 'Sonair', '2016-03-08', 76, 'Technology', 29477, 'Sound amplifiers as light as air.');
+
+INSERT INTO project VALUES ('65', 'Cardguard', '2016-05-11', 40, 'Entertainment', 2056, 'Store all your card games in this fancy device, you will never lose your cards again.');
+
+INSERT INTO project VALUES ('66', 'Opla', '2016-05-08', 51, 'Technology', 427689, 'Sing into this device and you will sound like an opera singer.');
+
+INSERT INTO project VALUES ('67', 'Lucky die', '2016-05-09', 73, 'Entertainment', 3464, 'A rigged die that will help you to cheat in all games.');
+
+INSERT INTO project VALUES ('68', 'Walleter', '2017-01-01', 34, 'Design', 20733, 'A wallet that reminds you to drink water at regular intervals. Water has to be drunk in order for you to open your wallet.');
+
+INSERT INTO project VALUES ('69', 'Water Ink', '2017-01-01', 91, 'Art', 22218, 'Ink that shows only when the medium is wet.');
+
+INSERT INTO project VALUES ('70', 'Sock it', '2016-09-07', 21, 'Fashion', 18987, 'Socks that double up as boxing gloves.');
+
+INSERT INTO project VALUES ('71', 'Trip-pledex', '2016-03-03', 43, 'Entertainment', 414531, 'Install this in your house, and invite your friends over. With a push of your button, the strings will be pulled taut and guaranteed to make your friends trip.');
+
+INSERT INTO project VALUES ('72', 'Voltsillam', '2016-04-27', 27, 'Entertainment', 4300, 'The upgraded version of the hand shake electrocuter.');
+
+INSERT INTO project VALUES ('73', 'Alpha', '2016-03-13', 96, 'Entertainment', 35900, 'Wearable device that shouts encouragement at you.');
+
+INSERT INTO project VALUES ('74', 'Sketcher', '2016-11-23', 74, 'Art', 1314, 'A Skecher themed sketch book.');
+
+INSERT INTO project VALUES ('75', 'Cool Trackers', '2016-07-24', 38, 'Fashion', 13958, 'Track pants which keep you cool and keep the mozzies away.');
+
+INSERT INTO project VALUES ('76', 'Fit flex', '2016-11-17', 20, 'Fashion', 4747, 'Well-fitted shirts designed to make your muscles look flexed all the time.');
+
+INSERT INTO project VALUES ('77', 'Bike locker', '2016-04-13', 16, 'Technology', 351303, 'The full bike-locker armed with an alarm, ensuring that your bike / bike-parts never gets stolen again.');
+
+INSERT INTO project VALUES ('78', 'Mat', '2016-10-22', 12, 'Design', 3018, 'The ultimate picnic mat which comes with everything you will need.');
+
+INSERT INTO project VALUES ('79', 'Jerry', '2016-05-28', 47, 'Technology', 354215, 'The flattest mouse ever, which fits snugly into the indent of your Macbook trackpad.');
+
+INSERT INTO project VALUES ('80', 'Tin', '2016-10-21', 65, 'Design', 18466, 'The fanciest tin you have ever seen.');
+
 INSERT INTO project VALUES ('81', 'SMOVE: Smartphone Stabilizer / Powerback in One', '2016-02-12', 100, 'Technology', 100000);
 
 INSERT INTO project VALUES ('82', 'ASAP Connect: The Future of USB Cables', '2016-01-18', 20, 'Technology', 75000);
