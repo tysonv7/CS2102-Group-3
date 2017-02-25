c106f2f9a515a2d90a1461e3acd64577ae450ce3
tysonv7 <tyson_v7@hotmail.com>
2017-02-25 22:05:41 +0800

    Added SampleBackers with back data for Projects 41-100

---
+++ Backend/TestData/SampleBackers.sql
@@ -0,0 +1,119 @@
+INSERT INTO back VALUES ('mgutierrezno', '41', 677);
+
+INSERT INTO back VALUES ('gfostergq', '42', 514);
+
+INSERT INTO back VALUES ('wmason1j', '43', 401);
+
+INSERT INTO back VALUES ('ifuller8v', '44', 517);
+
+INSERT INTO back VALUES ('cmartinez9h', '45', 652);
+
+INSERT INTO back VALUES ('mromero48', '46', 916);
+
+INSERT INTO back VALUES ('hkingda', '47', 928);
+
+INSERT INTO back VALUES ('tlarson1t', '48', 793);
+
+INSERT INTO back VALUES ('adavisjd', '49', 886);
+
+INSERT INTO back VALUES ('jmcdonaldd4', '50', 984);
+
+INSERT INTO back VALUES ('ibell1g', '51', 978);
+
+INSERT INTO back VALUES ('tlarson1t', '52', 376);
+
+INSERT INTO back VALUES ('kwarren5m', '53', 703);
+
+INSERT INTO back VALUES ('pgibsonf0', '54', 677);
+
+INSERT INTO back VALUES ('egrahamf9', '55', 133);
+
+INSERT INTO back VALUES ('dknight3v', '56', 829);
+
+INSERT INTO back VALUES ('sreid2g', '57', 480);
+
+INSERT INTO back VALUES ('ssanders4e', '58', 566);
+
+INSERT INTO back VALUES ('nthomasc2', '59', 149);
+
+INSERT INTO back VALUES ('shansonrd', '60', 215);
+
+INSERT INTO back VALUES ('ksimmonsh2', '61', 649);
+
+INSERT INTO back VALUES ('rrobertsm4', '62', 847);
+
+INSERT INTO back VALUES ('rharper7v', '63', 727);
+
+INSERT INTO back VALUES ('dpeterscq', '64', 534);
+
+INSERT INTO back VALUES ('krobinsonid', '65', 927);
+
+INSERT INTO back VALUES ('swilliams7j', '66', 781);
+
+INSERT INTO back VALUES ('jedwardsgh', '67', 431);
+
+INSERT INTO back VALUES ('dfranklin1k', '68', 915);
+
+INSERT INTO back VALUES ('lmason5d', '69', 655);
+
+INSERT INTO back VALUES ('vgreenej5', '70', 493);
+
+INSERT INTO back VALUES ('fgreen46', '71', 198);
+
+INSERT INTO back VALUES ('kyounghf', '72', 951);
+
+INSERT INTO back VALUES ('bwallacejv', '73', 647);
+
+INSERT INTO back VALUES ('emarshallgp', '74', 401);
+
+INSERT INTO back VALUES ('bschmidto6', '75', 622);
+
+INSERT INTO back VALUES ('ppalmerov', '76', 409);
+
+INSERT INTO back VALUES ('rlynch3p', '77', 190);
+
+INSERT INTO back VALUES ('akelleym6', '78', 751);
+
+INSERT INTO back VALUES ('spierceeb', '79', 710);
+
+INSERT INTO back VALUES ('kcarpenter1b', '80', 423);
+
+INSERT INTO back VALUES ('adiazon', '81', 272);
+
+INSERT INTO back VALUES ('dkelly3b', '82', 939);
+
+INSERT INTO back VALUES ('jhenryly', '83', 567);
+
+INSERT INTO back VALUES ('mfergusonpo', '84', 972);
+
+INSERT INTO back VALUES ('carmstrongib', '85', 999);
+
+INSERT INTO back VALUES ('srobertsoneg', '86', 686);
+
+INSERT INTO back VALUES ('kthomashe', '87', 631);
+
+INSERT INTO back VALUES ('mlopezan', '88', 41);
+
+INSERT INTO back VALUES ('dpeterson1r', '89', 915);
+
+INSERT INTO back VALUES ('dsullivan3', '90', 394);
+
+INSERT INTO back VALUES ('amatthewse8', '91', 301);
+
+INSERT INTO back VALUES ('carmstrongib', '92', 8);
+
+INSERT INTO back VALUES ('tnicholsjy', '93', 172);
+
+INSERT INTO back VALUES ('jmorales9w', '94', 659);
+
+INSERT INTO back VALUES ('grobinsonf', '95', 753);
+
+INSERT INTO back VALUES ('amurphy55', '96', 903);
+
+INSERT INTO back VALUES ('hkingda', '97', 561);
+
+INSERT INTO back VALUES ('barnoldc', '98', 91);
+
+INSERT INTO back VALUES ('rjames4u', '99', 966);
+
+INSERT INTO back VALUES ('gporterck', '100', 84);
