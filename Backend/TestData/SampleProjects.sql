6a0883b588ec1d87b14ec5a6a8fdb90b3c8b1968
Terence Kong <kong_terence@hotmail.com>
2017-02-25 22:49:26 +0800

    added projects 1-40

--- Backend/TestData/SampleProjects.sql
+++ Backend/TestData/SampleProjects.sql
@@ -1,3 +1,83 @@
+INSERT INTO project VALUES ('1', 'AIR-INK', '2017-02-07', 30, 'Technology', 14000, 'ink made of air pollution');
+
+INSERT INTO project VALUES ('2', 'SEA-INK', '2017-02-07', 30, 'Technology', 14000, 'ink made of sea pollution');
+
+INSERT INTO project VALUES ('3', 'LAND-INK', '2017-02-07', 30, 'Technology', 14000, 'ink made of land pollution.');
+
+INSERT INTO project VALUES ('4', 'Wonder Wickets', '2016-07-01', 21, 'Gaming', 420, 'A new spin on minigolf! Smash and curve your way through obstacles in a magical adventure!');
+
+INSERT INTO project VALUES ('5', 'WWII Online', '2016-07-02', 60, 'Gaming', 25000, 'Help us port WWII online to steam!');
+
+INSERT INTO project VALUES ('6', 'Lost Region Game', '2017-02-03', 30, 'Gaming', 20000, 'New survival MMO game powered by unreal engine 4.');
+
+INSERT INTO project VALUES ('7', 'Clutch baseball', '2017-02-05', 40, 'Gaming', 13000, 'Fast paced Fantasy baseball game.');
+
+INSERT INTO project VALUES ('8', 'Sidepad Mod', '2017-02-07', 30, 'Gaming', 15000, 'A super responsive moto mod gamepade for hardcore gamers!');
+
+INSERT INTO project VALUES ('9', 'Magus', '2017-02-08', 90, 'Gaming', 120000, 'Learn magic, cast spells and duel other wizards in the augmented reality world of magus.');
+
+INSERT INTO project VALUES ('10', 'Star Citizen', '2017-01-09', 120, 'Gaming', 140000000, 'Reclaim the stars in this space epic.');
+
+INSERT INTO project VALUES ('11', 'Writings of Bahlaullah', '2017-01-12', 60, 'Art', 3000, 'Stunning animal photographs and quotes from Bahaullah.');
+
+INSERT INTO project VALUES ('12', 'Inches', '2017-01-14', 30, 'Art', 500, 'A UNT Film Club production.');
+
+INSERT INTO project VALUES ('13', 'For a limited time', '2017-01-15', 40, 'Art', 4000, 'An experimental fashion everything.');
+
+INSERT INTO project VALUES ('14', 'May Days monograph', '2017-01-16', 40, 'Art', 15000, 'A visual journal made in Havana, Cuba.');
+
+INSERT INTO project VALUES ('15', 'Touch', '2017-01-25', 30, 'Art', 2987, 'An image of two hands touching one another.');
+
+INSERT INTO project VALUES ('16', 'Shadow', '2017-02-03', 60, 'Art', 2000, 'A short film.');
+
+INSERT INTO project VALUES ('17', 'Tory time', '2017-02-09', 7, 'Art', 10000, 'Help an artist get enough time to do things he wants to do.');
+
+INSERT INTO project VALUES ('18', '100 women and more', '2017-02-08', 60, 'Art', 10000, 'Book of portraits of 100 infuential women.');
+
+INSERT INTO project VALUES ('19', 'Glowing Kanji', '2017-02-16', 23, 'Art', 750, 'Japanese art. Made in Berlin.');
+
+INSERT INTO project VALUES ('20', 'Donkey museum of Tangier', '2017-02-20', 60, 'Art', 10000, 'Participez au musée de lâne, la galerie où se rencontrent : culture, gastronomie, histoire et art.');
+
+INSERT INTO project VALUES ('21', 'The Whimsel Loop', '2017-02-21', 30, 'Writing', 23000, 'A collaborative fantasy novel.');
+
+INSERT INTO project VALUES ('22', '9 years old', '2017-02-23', 30, 'Writing', 5000, 'A narrative on overcoming adversity.');
+
+INSERT INTO project VALUES ('23', 'Who ate the cake?', '2017-02-25', 120, 'Writing', 7000, 'A choose your own adventure book for encouraging decision making in young readers.');
+
+INSERT INTO project VALUES ('24', 'Visual Pathways The Book', '2016-05-12', 60, 'Writing', 6000, 'Become a master of your time - learn how to be productive.');
+
+INSERT INTO project VALUES ('25', 'Surviving the cure', '2016-06-11', 30, 'Writing', 10000, 'A post cancer memoir.');
+
+INSERT INTO project VALUES ('26', 'Help BrokeAssStuart.com', '2016-05-10', 120, 'Writing', 30000, 'Help an award winning independent media site grow to its full potential.');
+
+INSERT INTO project VALUES ('27', 'SheRa Mag', '2016-11-06', 60, 'Writing', 15000, 'For girls who think.');
+
+INSERT INTO project VALUES ('28', 'Starsong', '2016-09-17', 30, 'Writing', 6500, 'My book series.');
+
+INSERT INTO project VALUES ('29', 'Son of flight', '2016-08-10', 39, 'Writing', 758, 'A book for ages 8 and up. An immaginary tale of love and friendship');
+
+INSERT INTO project VALUES ('30', 'Poetry in flight', '2016-12-24', 60, 'Writing', 1500, 'Help fund the production costs of a poetry anthology.');
+
+INSERT INTO project VALUES ('31', 'Laura Macri solo album', '2016-02-18', 30, 'Music', 10000, 'Help an italian opera singer release her first album.');
+
+INSERT INTO project VALUES ('32', 'PreFuture debut album', '2016-05-10', 30, 'Music', 1000, 'Our first album needs your final push towards becoming a reality.');
+
+INSERT INTO project VALUES ('33', 'Songs from the North Country', '2016-05-11', 30, 'Music', 5000, 'An album inspired by 3 months in Norway.');
+
+INSERT INTO project VALUES ('34', 'ULALI project album', '2016-04-28', 60, 'Music', 67000, 'The long awaited ULALI Project first album!');
+
+INSERT INTO project VALUES ('35', 'Synergy Live', '2016-04-21', 60, 'Music', 10000, 'Experience Synergy LIVE.');
+
+INSERT INTO project VALUES ('36', 'Somebody lied to me music video', '2016-04-09', 30, 'Music', 2000, 'Song by Leaders of Da New World mother and son team.');
+
+INSERT INTO project VALUES ('37', 'We remember album', '2016-04-04', 30, 'Music', 700, 'Help hannah complete digital distribution to give away her album for FREE.');
+
+INSERT INTO project VALUES ('38', 'Dominant Collective Concert', '2016-03-18', 60, 'Music', 12000, 'This concert is a platform for young and upcoming artists to express their gifts.');
+
+INSERT INTO project VALUES ('39', 'Pacific Mambo Orchestra', '2016-08-03', 60, 'Music', 8500, 'The Grammy award winning Orchestra releases its 2nd full length album!');
+
+INSERT INTO project VALUES ('40', 'Colour Sound', '2016-02-03', 60, 'Music', 2000, 'Music delivery platform.');
+
 INSERT INTO project VALUES ('41', 'Voyatouch', '2017-02-11', 39, 'Technology', 456200, 'Drag this device across any surface and it will map out the surface texture on your laptop.');
 
 INSERT INTO project VALUES ('42', 'Tres-Zap', '2017-01-23', 53, 'Technology', 183648, 'The most efficient way of draining the sap from rubber trees');
