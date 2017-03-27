2d4b9aa9e34eb7fecc4b525492b8c4e79d3aa2e0
tysonv7 <tyson_v7@hotmail.com>
2017-03-27 13:49:31 +0800

    Added more queries

--- Backend/queries.sql
+++ Backend/queries.sql
@@ -1,3 +1,4 @@
+-- Simple
 -- Getting projects with funding needed > amount dictated by user
 -- @REPLACE number with user's value
 SELECT *
@@ -66,6 +67,40 @@ WHERE f.uid2 = 'kirigiri'
   AND b.pid = p.pid
 ORDER BY u.name, p.title;
 
+--Get the list of comments associated to a project
+SELECT c.content
+FROM Project p, Comment c
+WHERE c.pid = p.id;
+
+--Get the list of all comments made by a user
+SELECT c.content
+FROM Comment c, Users u
+WHERE c.uid = u.id;
+
+--Get the list of all comments made by a user on a project
+SELECT c.content
+FROM Project p, Comment c, Users u
+WHERE c.pid = p.id AND c.uid = u.id;
+
+--Get the list of users and ordered alphabetically
+SELECT u.id, u.name
+FROM Users u
+ORDER BY u.name ASC;
+
+--Get the list of users and ordered reversed alphabetically
+SELECT u.id, u.name
+FROM Users u
+GROUP BY u.name DESC;
+
+--Get the list of project and ordered alphabetically
+SELECT p.id, p.title, p.startDate, p.duration, p.category, p.fundNeeded
+FROM Project p
+ORDER BY p.title ASC;
+
+--Get the list of project and ordered reversed alphabetically
+SELECT p.id, p.title, p.startDate, p.duration, p.category, p.fundNeeded
+FROM Project p
+ORDER BY p.title DESC;
 
 -- AGGREGATE QUERIES
 
@@ -141,3 +176,62 @@ SELECT b.uid, COUNT(*) AS numProjectsBacked, SUM(amount) AS totalAmountBacked
 FROM Back b
 GROUP BY b.uid
 ORDER BY COUNT(*) DESC;
+
+--Count total number of backers for each project
+SELECT COUNT(b.pid)
+FROM Back b
+WHERE b.pid = '100'; --The '100' is a stub. Must change to user input
+
+--Count total number of projects a person has backed
+SELECT COUNT(b.uid)
+FROM Back b
+WHERE b.uid = 'tysonv7'; -- The 'tysonv7' is a stub. Must change to user input
+
+--GROUP the projects a user has backed by successful
+
+--GROUP the projects a user has backed by unsuccessful
+
+--Admin can view total amount of funding for all projects
+SELECT b.pid, p.title, SUM(b.amount)
+FROM Back b, Project p
+WHERE b.pid = p.id
+GROUP BY b.pid, p.title
+ORDER BY p.title ASC;
+
+--Admin can see average how many projects a person backed
+SELECT COUNT(b.pid)/COUNT(b.uid) AS AverageNumberOfProjectsBackedPerPerson
+FROM BACK b;
+
+--Admin can see average backing a project receive
+SELECT SUM(b.amount)/COUNT(b.uid) AS AverageAmountOfBackingPerProject
+FROM Back b;
+
+--Admin can see average number of projects created by user
+SELECT COUNT(s.pid)/COUNT(s.uid) AS AverageNumberOfProjectsCreatedPerUser
+FROM Start s;
+
+--Nested Queries
+--Admin can see a list of how many users have backed a project
+SELECT u.id, u.name
+FROM Users u
+WHERE u.id = ANY (SELECT b.uid FROM Back b);
+
+--Admin can see a list of how many users who have started a project
+SELECT u.id, u.name
+FROM Users u
+WHERE u.id = ANY (SELECT s.uid FROM Start s);
+
+--Algebraic
+--Admin can get list of what users have started and backed
+SELECT u.id AS Username, u.name, CASE WHEN s.pid IS NULL THEN 'Have not started any project' ELSE s.pid END AS StartedProject, CASE WHEN b.pid IS NULL THEN 'Have not backed any project' ELSE b.pid END AS BackedProject, b.amount
+FROM USERS u LEFT OUTER JOIN Start s ON u.id = s.uid LEFT OUTER JOIN Back b ON u.id = b.uid
+GROUP BY u.id, s.pid, b.pid, b.amount
+ORDER BY u.id;
+
+--Admin can get list of what users have started and backed, specified to which category of projects
+SELECT u.id AS Username, u.name, CASE WHEN s.pid IS NULL THEN 'Have not started any project' ELSE s.pid END AS StartedProjectID, CASE WHEN b.pid IS NULL THEN 'Have not backed any project' ELSE b.pid END AS BackedProjectID, b.amount
+FROM USERS u LEFT OUTER JOIN Start s ON u.id = s.uid LEFT OUTER JOIN Back b ON u.id = b.uid, Project p
+WHERE (s.pid = p.id AND p.category = 'Technology') OR (b.uid = p.id AND p.category = 'Technology') --'Technology' is a stub. Need to replace with users input
+GROUP BY u.id, s.pid,p.title, b.pid, b.amount
+ORDER BY u.id;
+
